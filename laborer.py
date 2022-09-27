b296892c6cc7e3ff6bc7f50b01254bfe5c38d5c1
Timchenko <tbd2504@gmail.com>
2022-09-27 16:11:54 +0300

    module_laborer-2.0

--- laborer.py
+++ laborer.py
@@ -3,7 +3,7 @@ from decimal import Decimal
 import wx
 
 import config as cfg
-from helped import MyDialog, InterfaceListModel, InterfaceListView, MyPanel, print_indicator
+from helped import MyDialog, InterfaceListModel, InterfaceListView, MyPanel
 import Language.en as lang
 
 import main_container
@@ -36,10 +36,10 @@ class Laborer:
         return self.__str__()
 
 
-class ListModel:
+class ListModel(InterfaceListModel):
     def __init__(self):
-        self.laborers: dict[int: Laborer] = main_container.laborer
-        self.ids: dict[str: int] = {}
+        super().__init__()
+        self.objects: dict[int: Laborer] = main_container.laborer
 
     @staticmethod
     def create(full_name: str, job_title: str, payment: str, rate: str) -> Laborer:
@@ -51,38 +51,29 @@ class ListModel:
         laborer.rate = rate
         return laborer
 
-    def get_name_list(self) -> list[str]:
-        """Повертає список імен Laborer.full_name."""
-        return [*self.ids.keys()]
-
-    def get_laborer(self, select_id: int) -> Laborer:
-        """Повертає Laborer за індексом."""
-        return self.laborers.get(select_id, 'None ID')
-
     def get_name(self, select_id: int) -> str:
         """Повертає ім'я за індексом"""
-        return self.get_laborer(select_id).full_name
-
-    def get_id(self, select_name: str) -> int:
-        """Повертає індекс за ім'ям."""
-        return self.ids.get(select_name, 'None Name')
+        return self.get_object(select_id).full_name
 
     def add(self, laborer: Laborer):
-        """Додає Laborer у словники:
-        laborers[Laborer.id: Laborer]
-        names[Laborer.id: Laborer.full_name]
+        """Додає Object у словники:
+        objects[id: object]
+        ids[name: id]
         """
-        self.__add_in_laborers(laborer)
-        self.__add_in_ids(laborer)
+        self.add_in_objects(laborer.id, laborer)
+        self.add_in_ids(laborer.full_name, laborer.id)
 
-    def delete(self, select_id: int, select_name: str):
-        """Видаляє Laborer та його атрибути з усіх словників"""
-        self.__del_laborer(select_id)
-        self.__del_id(select_name)
+    def set_value_widgets(self, select_id: int, full_name: wx.TextCtrl, job_title: wx.ComboBox, payment: wx.ComboBox, rate: wx.TextCtrl):
+        """Встановлює у wx_widgets значення атрибутів Laborer."""
+        laborer = self.get_object(select_id)
+        full_name.SetValue(laborer.full_name)
+        job_title.SetValue(laborer.job_title)
+        payment.SetValue(laborer.payment)
+        rate.SetValue(str(laborer.rate))
 
     def set_attrs_select_laborer(self, select_id: int, full_name: str, job_title: str, payment: str, rate: str):
         """Встановлює атрибути вибраного Laborer"""
-        laborer = self.get_laborer(select_id)
+        laborer = self.get_object(select_id)
         laborer.full_name = full_name
         laborer.job_title = job_title
         laborer.payment = payment
@@ -93,38 +84,8 @@ class ListModel:
         old_name = select_name
         new_name = self.get_name(select_id)
         if old_name != new_name:
-            self.__del_id(select_name)
-            self.__add_in_ids(self.get_laborer(select_id))
-
-    def set_value_widgets(self, select_id: int, full_name: wx.TextCtrl, job_title: wx.ComboBox, payment: wx.ComboBox, rate: wx.TextCtrl):
-        """Встановлює у wx_widgets значення атрибутів Laborer."""
-        laborer = self.get_laborer(select_id)
-        full_name.SetValue(laborer.full_name)
-        job_title.SetValue(laborer.job_title)
-        payment.SetValue(laborer.payment)
-        rate.SetValue(str(laborer.rate))
-
-    def is_long_laborers_equal_one(self) -> bool:
-        if len(self.laborers) == 1:
-            return True
-        return False
-
-    def is_empty_laborers(self) -> bool:
-        if len(self.laborers) <= 0:
-            return True
-        return False
-
-    def __add_in_laborers(self, laborer: Laborer):
-        self.laborers.setdefault(laborer.id, laborer)
-
-    def __add_in_ids(self, laborer: Laborer):
-        self.ids.setdefault(laborer.full_name, laborer.id)
-
-    def __del_laborer(self, select_id: int):
-        del self.laborers[select_id]
-
-    def __del_id(self, select_name: str):
-        del self.ids[select_name]
+            self.add_in_ids(new_name, self.get_id(old_name))
+            self.del_id(old_name)
 
 
 class LaborerInfoView(MyDialog):
@@ -215,101 +176,22 @@ class LaborerView(MyDialog):
         return self.tc_full_name.GetValue(), self.cb_job_title.GetValue(), self.cb_payment.GetValue(), self.tc_rate.GetValue()
 
 
-class LaborerListView(MyPanel):
+class LaborerListView(InterfaceListView):
     def __init__(self, parent, model: ListModel):
-        super().__init__(parent)
-        # Variables
-        self.parent = parent
-        self.select_id = None
-        self.select_name = None
-        self.editing = False
-        self.model = model
+        super().__init__(parent, model)
 
-        # Widgets
-        st_title = wx.StaticText(self, label=lang.NAME_TITLE['laborer'])
-        self.list_box = wx.ListBox(self, size=(210, 270), choices=self.model.get_name_list())
-        self.btn_info = wx.Button(self, id=wx.ID_ANY, label=lang.NAME_BTN['info'])
-        self.btn_add = wx.Button(self, id=wx.ID_ANY, label=lang.NAME_BTN['add'])
-        self.btn_edit = wx.Button(self, id=wx.ID_ANY, label=lang.NAME_BTN['edit'])
-        self.btn_del = wx.Button(self, id=wx.ID_ANY, label=lang.NAME_BTN['del'])
-
-        # Sizers
-        main_box = wx.BoxSizer(wx.VERTICAL)
-        main_box.Add(st_title, flag=wx.ALIGN_CENTRE | wx.TOP | wx.BOTTOM, border=10)
-        main_box.Add(self.list_box, flag=wx.ALIGN_CENTRE | wx.BOTTOM, border=10)
-        main_box.Add(self.btn_info, flag=wx.ALIGN_CENTRE | wx.BOTTOM, border=10)
-        main_box.Add(self.btn_add, flag=wx.ALIGN_CENTRE | wx.BOTTOM, border=10)
-        main_box.Add(self.btn_edit, flag=wx.ALIGN_CENTRE | wx.BOTTOM, border=10)
-        main_box.Add(self.btn_del, flag=wx.ALIGN_CENTRE | wx.BOTTOM, border=10)
-
-        self.SetSizer(main_box)
-
-        # Binds
-        self.Bind(wx.EVT_BUTTON, self.on_info, self.btn_info)
-        self.Bind(wx.EVT_BUTTON, self.on_add, self.btn_add)
-        self.Bind(wx.EVT_BUTTON, self.on_delete, self.btn_del)
-        self.Bind(wx.EVT_BUTTON, self.on_edit, self.btn_edit)
-        self.Bind(wx.EVT_LISTBOX, self.on_select)
-
-        # Methods
-        self.disable_buttons()
-
-    def on_select(self, event):
-        index = self.list_box.GetSelection()
-        self.select_name = self.model.get_name_list()[index]
-        self.select_id = self.model.get_id(self.select_name)
-
-    def on_info(self, event):
-        if self.select_id is not None:
-            laborer = self.model.get_laborer(self.select_id)
-            LaborerInfoView(self.parent, laborer).ShowModal()
-
-    def on_add(self, event):
-        self.editing = False
-        self.show_default_laborer_view()
-        self.fulled_list()
-        self.enable_buttons()
-
-    def on_edit(self, event):
-        if self.select_id is not None:
-            self.editing = True
-            self.show_default_laborer_view()
-            self.fulled_list()
-
-    def on_delete(self, event):
-        if self.select_id is not None:
-            self.model.delete(self.select_id, self.select_name)
-            self.select_id = None
-            self.select_name = None
-            self.fulled_list()
-            self.disable_buttons()
-
-    def fulled_list(self):
-        """Заповнює list_box"""
-        self.list_box.SetItems(self.model.get_name_list())
-
-    def enable_buttons(self):
-        """Вмикає кнопки info, edit та del якщо в laborers є 1 елемент."""
-        if self.model.is_long_laborers_equal_one():
-            self.btn_info.Enable(True)
-            self.btn_edit.Enable(True)
-            self.btn_del.Enable(True)
-
-    def disable_buttons(self):
-        """Вимикає кнопки info, edit та del якщо laborers порожній."""
-        if self.model.is_empty_laborers():
-            self.btn_info.Enable(False)
-            self.btn_edit.Enable(False)
-            self.btn_del.Enable(False)
-
-    def show_default_laborer_view(self):
-        """Показує діалогове вікно заповнене за замовчуванням"""
+    def show_default_object_view(self):
+        """Показує діалогове вікно Object заповнене за замовчуванням"""
         laborer_view = LaborerView(self.parent, self.model, self.select_id, self.select_name, self.editing)
         laborer_view.ShowModal()
 
-    def show_fulled_laborer_view(self):
-        """Показує діалогове вікно заповнене атрибутами вибраного Laborer."""
+    def show_fulled_object_view(self):
+        """Показує діалогове вікно заповнене атрибутами вибраного Object."""
         laborer_view = LaborerView(self.parent, self.model, self.select_id, self.select_name, self.editing)
         self.model.set_value_widgets(self.select_id, *laborer_view.get_all_widgets())
         laborer_view.enable_tc_rate()
         laborer_view.ShowModal()
+
+    def show_info_view(self):
+        """Показує діалогове вікно заповнене інформацією вибраного Object."""
+        LaborerInfoView(self.parent, self.model.get_object(self.select_id)).ShowModal()
